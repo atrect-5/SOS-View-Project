7b7b0f9e4c861ba0a229ed1e8abca337a3ec526e
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-02-27 16:13:29 -0600

    change folder name machineForm -> companyForm

--- src/components/components.jsx
+++ src/components/components.jsx
@@ -2,7 +2,7 @@
 import UserLogin from "./login/login"
 import HomePage from "./homepage/homepage"
 import UserRegister from "./forms/userForm/userForm"
-import CompanyRegister from "./forms/machineForm/companyForm"
+import CompanyRegister from "./forms/companyForm/companyForm"
 
 export {
     HomePage,
